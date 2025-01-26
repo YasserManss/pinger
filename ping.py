f74ba5e5fa82b347f798a6b3b6c5a51a8f5c63a0
root <root@yas>
2025-01-26 14:15:23 +0300

    a

--- ping.py
+++ ping.py
@@ -6,11 +6,14 @@ def load_hosts():
         hosts = yaml.safe_load(file)
     return hosts
 
+def offline(host):
+    print(f"host {host} is offline!")
+
 def ping_hosts(hosts):
     for host in hosts:
         p = ping(target=host['IP'], timeout=0.2, count=3)
         if not p.success():
-            print(f"host {host['description']} not pingable!")
+            offline(host)
 
 if __name__ == "__main__":
     hosts = load_hosts()
